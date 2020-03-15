500c9bdab4f7a4d4a561449203b1ce567786befb
Administrator <nysyxxg@163.com>
2020-03-15 10:46:46 +0800

    Callable

--- src/main/java/com/chapter6/TestExecutor.java
+++ src/main/java/com/chapter6/TestExecutor.java
@@ -2,14 +2,18 @@ package com.chapter6;
 
 import java.util.concurrent.*;
 
+/**
+ * Callable 的使用
+ */
 public class TestExecutor {
     public static void main(String args[]){
         ExecutorService executorService = Executors.newFixedThreadPool(1);
         test2(executorService);
+
+        executorService.shutdown();
     }
 
     public static void test1(ExecutorService executorService){
-
         executorService.submit(new Runnable() {
             @Override
             public void run() {
@@ -19,18 +23,20 @@ public class TestExecutor {
     }
 
     public static void test2(ExecutorService executorService){
-        Future future = executorService.submit(new Callable() {
-            @Override
-            public Object call() throws Exception {
-                return "测试结果";
-            }
-        });
+        Future future = executorService.submit(new MyTestTask() );
         try {
-            System.out.println("future结果"+future.get());
+            System.out.println("future结果 "+future.get());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static class MyTestTask implements  Callable{
+        @Override
+        public Object call() throws Exception {
+            return "测试结果";
         }
     }
 }
