ef522fca67cc914fd752687fc2196576f6345b96
Administrator <nysyxxg@163.com>
2020-03-15 09:23:10 +0800

    完善代码

--- src/main/java/com/chapter2/FirstReentrantLock.java
+++ src/main/java/com/chapter2/FirstReentrantLock.java
@@ -3,33 +3,33 @@ package com.chapter2;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
+ * ReentrantLock 的简单实用
  * @author tangj
- * 
  */
 public class FirstReentrantLock {
 
-	public static void main(String[] args) {
-		Runnable runnable = new ReentrantLockThread();
-		new Thread(runnable, "a").start();
-		new Thread(runnable, "b").start();
-	}
+    public static void main(String[] args) {
+        Runnable runnable = new ReentrantLockThread();
+        new Thread(runnable, "a").start();
+        new Thread(runnable, "b").start();
+    }
 
 }
 
 class ReentrantLockThread implements Runnable {
-	ReentrantLock lock = new ReentrantLock();
+    ReentrantLock lock = new ReentrantLock();
 
-	@Override
-	public void run() {
-		try {
-			lock.lock();
-			for (int i = 0; i < 3; i++) {
-				System.out.println(Thread.currentThread().getName()  + i);
-			}
-		} finally {
-			lock.unlock();
-		}
-
-	}
-
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " 线程，获得锁，开始执行。");
+            for (int i = 0; i < 3; i++) {
+                System.out.println(Thread.currentThread().getName() + i);
+            }
+            System.out.println(Thread.currentThread().getName() + " 线程，获得锁，开始完成。");
+        } finally {
+            lock.unlock();
+        }
+    }
 }
