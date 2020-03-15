3ff2b0b3c50a5ac5d0d3c09ca058474a9a3c0772
Administrator <nysyxxg@163.com>
2020-03-15 10:05:21 +0800

    ThreadPoolExecutor

--- src/main/java/com/chapter3/threadpool/ExtThreadPool.java
+++ src/main/java/com/chapter3/threadpool/ExtThreadPool.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ExtThreadPool {
+
     public static class MyTask2 implements Runnable{
         public String name;
         public MyTask2(String name){
@@ -13,11 +14,11 @@ public class ExtThreadPool {
         }
         @Override
         public void run() {
-            System.out.println(System.currentTimeMillis()+"Thrad ID:"+Thread.currentThread().getId());
+            System.out.println(System.currentTimeMillis()+" Thrad ID:"+Thread.currentThread().getId());
             try {
-                Thread.sleep(100);
-                System.out.println("正在执行的任务"+name);
-            } catch (InterruptedException e) {
+                 Thread.sleep(100);
+                System.out.println(System.currentTimeMillis()+" Thrad ID:"+Thread.currentThread().getId() + "  正在执行的任务"+name);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -27,16 +28,16 @@ public class ExtThreadPool {
         ExecutorService es = new ThreadPoolExecutor(5,5,0L, TimeUnit.MICROSECONDS,new LinkedBlockingQueue<Runnable>()){
           @Override
           protected void beforeExecute(Thread t, Runnable r) {
-              System.out.println("准备执行: "+((MyTask2)r).name );
+              System.out.println("准备执行: "+((MyTask2)r).name );  // 线程执行前，打印输出
           }
 
           @Override
           protected void afterExecute(Runnable r, Throwable t) {
-              System.out.println("执行完成: "+((MyTask2)r).name );
+              System.out.println("执行完成: "+((MyTask2)r).name ); // 线程执行后
           }
 
         };
-        for (int i = 0;i<5;i++){
+        for (int i = 0;i<5;i++){// 创建5个线程，提交到线程池，开始执行
             MyTask2 task2 = new MyTask2("Task "+i);
             es.execute(task2);
             Thread.sleep(10);
